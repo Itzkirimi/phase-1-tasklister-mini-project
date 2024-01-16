52f9f59269b31cbd45fcf8702455a5688c4b4bea
Itzkirimi <kirimihansel@gmail.com>
2024-01-16 03:56:30 +0300

    Done with Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("form#create-task-form")
+  const inputDesc = document.querySelector('form > input#new-task-description')
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    console.log(inputDesc.value)
+  })
 });
+// delete button
+const deleteBtn = document.createElement('button')
+deleteBtn.innerText = '❌'
+newLi.append(deleteBtn)
+
+
+//delete button function
+deleteBtn.addEventListener("click", (e) => {
+  const removingLi = e.target.parentNode
+  removingLi.remove()
+  newLi.remove()
+})
